1f1b7d67172c66afc7b71369592d917ae5a0eab3
binghan <binghan60@gmail.com>
2022-07-20 16:33:33 +0800

    JWT測試

--- index.js
+++ index.js
@@ -2,30 +2,27 @@ require("dotenv").config();
 
 // 拿到一個function
 const express = require("express");
-const multer = require('multer');
+const multer = require("multer");
 // multer檔案上傳,dest目的地
 // const upload = multer({ dest: 'tmp-uploads' });因為要用storage跟filefilter所以改用自己寫的
-const upload = require(__dirname + '/modules/upload-images');
+const upload = require(__dirname + "/modules/upload-images");
 
-const session = require('express-session');
-const moment = require('moment-timezone');
-const axios = require('axios');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
+const session = require("express-session");
+const moment = require("moment-timezone");
+const axios = require("axios");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
-
-const {
-    toDateString,
-    toDatetimeString,
-} = require(__dirname + '/modules/date-tools');
+const { toDateString, toDatetimeString } = require(__dirname +
+  "/modules/date-tools");
 
 // sessionStore
-const db = require(__dirname + '/modules/mysql-connect');
-const MysqlStore = require('express-mysql-session')(session);
+const db = require(__dirname + "/modules/mysql-connect");
+const MysqlStore = require("express-mysql-session")(session);
 // 官方文件const sessionStore = new MysqlStore(option);option是連線設定檔
 // 第一個參數連線設定,第二個參數連線物件
 const sessionStore = new MysqlStore({}, db);
-const cors = require('cors')
+const cors = require("cors");
 
 // 也可以這樣寫
 // const app = require("express")();
@@ -35,243 +32,250 @@ const app = express();
 // 建立view引擎
 app.set("view engine", "ejs");
 // 區分大小寫
-app.set('case sensitive routing', true);
+app.set("case sensitive routing", true);
 
 // 頂級路由設在最上層,所有資料都會經過這裡
 // Top-level middlewares
 const corsOptions = {
-    credentials: true,
-    origin: (origin, cb) => {
-        console.log({ origin });
-        //全部允許
-        cb(null, true);
-    }
+  credentials: true,
+  origin: (origin, cb) => {
+    console.log({ origin });
+    //全部允許
+    cb(null, true);
+  },
 };
 
 app.use(cors(corsOptions));
-app.use(session({
+app.use(
+  session({
     saveUninitialized: false,
     // 沒變更要不要回存
     resave: false,
     // 加密cookie用字串
-    secret: 'milkcat',
+    secret: "milkcat",
     store: sessionStore,
     // cookie存活時間
     cookie: {
-        maxAge: 1800000, // 30 min
-    }
-}));
+      maxAge: 1800000, // 30 min
+    },
+  })
+);
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use((req, res, next) => {
-    res.locals.shinder = '哈囉';
+  res.locals.shinder = "哈囉";
 
-    // template helper functions
-    res.locals.toDateString = toDateString;
-    res.locals.toDatetimeString = toDatetimeString;
-    res.locals.session = req.session;
-    next();
+  // template helper functions
+  res.locals.toDateString = toDateString;
+  res.locals.toDatetimeString = toDatetimeString;
+  res.locals.session = req.session;
+
+  const auth = req.get("Authorization");
+  res.locals.payload = null;
+  if (auth && auth.indexOf("Bearer ") === 0) {
+    const token = auth.slice(7);
+    res.locals.payload = jwt.verify(token, process.env.JWT_SECRET);
+  }
+
+  next();
 });
 
 // .get接受get的方法
-app.get('/try-qs', (req, res) => {
-    res.json(req.query);
+app.get("/try-qs", (req, res) => {
+  res.json(req.query);
 });
 
 // middleware: 中介軟體 是一個(function)
 // const bodyParser = express.urlencoded({extended: false});
-app.post('/try-post', (req, res) => {
-    res.json(req.body);
+app.post("/try-post", (req, res) => {
+  res.json(req.body);
 });
 
-app.route('/try-post-form')
-    .get((req, res) => {
-        res.render('try-post-form');
-    })
-    .post((req, res) => {
-        const { email, password, name } = req.body;
-        res.render('try-post-form', { email, password, name });
-    });
+app
+  .route("/try-post-form")
+  .get((req, res) => {
+    res.render("try-post-form");
+  })
+  .post((req, res) => {
+    const { email, password, name } = req.body;
+    res.render("try-post-form", { email, password, name });
+  });
 // 欄位名稱 avatar middleware,其他欄位的資料會放在req.body裡一起傳過來
-app.post('/try-upload', upload.single('avatar'), (req, res) => {
-    res.json(req.file);
+app.post("/try-upload", upload.single("avatar"), (req, res) => {
+  res.json(req.file);
 });
 // 欄位名稱 photos middleware
-app.post('/try-uploads', upload.array('photos'), (req, res) => {
-    res.json(req.files);
+app.post("/try-uploads", upload.array("photos"), (req, res) => {
+  res.json(req.files);
 });
 
 // 有:action跟:id就一定要有值,直接去/try-params1會404,有加問號才可以可有可無
-app.get('/try-params1/:action/:id', (req, res) => {
-    res.json({ code: 2, params: req.params });
-})
-app.get('/try-params1/:action', (req, res) => {
-    res.json({ code: 3, params: req.params });
-})
-app.get('/try-params1/:action?/:id?', (req, res) => {
-    res.json({ code: 1, params: req.params });
+app.get("/try-params1/:action/:id", (req, res) => {
+  res.json({ code: 2, params: req.params });
+});
+app.get("/try-params1/:action", (req, res) => {
+  res.json({ code: 3, params: req.params });
+});
+app.get("/try-params1/:action?/:id?", (req, res) => {
+  res.json({ code: 1, params: req.params });
 });
 
 //  跳脫設定路由,/後有包含HI就可以
 app.get(/^\/hi\/?/i, (req, res) => {
-    res.send({ url: req.url });
+  res.send({ url: req.url });
 });
-app.get(['/aaa', '/bbb'], (req, res) => {
-    res.send({ url: req.url, code: 'array' });
-});
-
-app.get('/try-json', (req, res) => {
-    // require會自動jsonParse轉成array    
-    const data = require(__dirname + '/data/data01');
-    console.log(data);
-    // rows掛到locals下前端就有這個變數了
-    res.locals.rows = data;
-    res.render('try-json');
+app.get(["/aaa", "/bbb"], (req, res) => {
+  res.send({ url: req.url, code: "array" });
 });
 
-
-app.get('/try-moment', (req, res) => {
-    const fm = 'YYYY-MM-DD HH:mm:ss';
-    const m1 = moment();
-    const m2 = moment('2022-02-28');
-
-    res.json({
-        m1: m1.format(fm),
-        // tz切換時區
-        m1a: m1.tz('Europe/London').format(fm),
-        m2: m2.format(fm),
-        m2a: m2.tz('Europe/London').format(fm),
-    })
+app.get("/try-json", (req, res) => {
+  // require會自動jsonParse轉成array
+  const data = require(__dirname + "/data/data01");
+  console.log(data);
+  // rows掛到locals下前端就有這個變數了
+  res.locals.rows = data;
+  res.render("try-json");
 });
 
+app.get("/try-moment", (req, res) => {
+  const fm = "YYYY-MM-DD HH:mm:ss";
+  const m1 = moment();
+  const m2 = moment("2022-02-28");
 
+  res.json({
+    m1: m1.format(fm),
+    // tz切換時區
+    m1a: m1.tz("Europe/London").format(fm),
+    m2: m2.format(fm),
+    m2a: m2.tz("Europe/London").format(fm),
+  });
+});
 
-const adminsRouter = require(__dirname + '/routes/admins');
+const adminsRouter = require(__dirname + "/routes/admins");
 // prefix 前綴路徑admins
-app.use('/admins', adminsRouter);
+app.use("/admins", adminsRouter);
 app.use(adminsRouter);
 // app.use('/admins', require(__dirname + '/routes/admins'));
 
+app.get("/try-session", (req, res) => {
+  // 如果有my_var就顯示,不然就給0
+  req.session.my_var = req.session.my_var || 0;
+  req.session.my_var++;
+  res.json({
+    my_var: req.session.my_var,
+    session: req.session,
+  });
+});
 
-app.get('/try-session', (req, res) => {
-    // 如果有my_var就顯示,不然就給0
-    req.session.my_var = req.session.my_var || 0;
-    req.session.my_var++;
-    res.json({
-        my_var: req.session.my_var,
-        session: req.session,
-    });
-})
+app.use("/address-book", require(__dirname + "/routes/address-book"));
 
-app.use('/address-book', require(__dirname + '/routes/address-book'));
+app.use("/carts", require(__dirname + "/routes/carts"));
 
-app.use('/carts', require(__dirname + '/routes/carts'));
-
-app.get('/yahoo', async (req, res) => {
-    axios.get('https://tw.yahoo.com/')
-        .then(function (response) {
-            // handle success
-            console.log(response);
-            res.send(response.data);
-        })
+app.get("/yahoo", async (req, res) => {
+  axios.get("https://tw.yahoo.com/").then(function (response) {
+    // handle success
+    console.log(response);
+    res.send(response.data);
+  });
 });
 
 //登入
-app.route('/login')
-    .get(async (req, res) => {
-        res.render('login');
-    })
-    .post(async (req, res) => {
-        const output = {
-            success: false,
-            error: '',
-            code: 0,
-        };
-        const sql = "SELECT * FROM admins WHERE account=?";
-        const [r1] = await db.query(sql, [req.body.account]);
+app
+  .route("/login")
+  .get(async (req, res) => {
+    res.render("login");
+  })
+  .post(async (req, res) => {
+    const output = {
+      success: false,
+      error: "",
+      code: 0,
+    };
+    const sql = "SELECT * FROM admins WHERE account=?";
+    const [r1] = await db.query(sql, [req.body.account]);
 
-        if (!r1.length) {
-            // 帳號錯誤
-            output.code = 401;
-            output.error = '帳密錯誤'
-            return res.json(output)
-        }
-        //const row = r1[0];
+    if (!r1.length) {
+      // 帳號錯誤
+      output.code = 401;
+      output.error = "帳密錯誤";
+      return res.json(output);
+    }
+    //const row = r1[0];
 
-        output.success = await bcrypt.compare(req.body.password, r1[0].pass_hash);
-        if (!output.success) {
-            // 密碼錯誤
-            output.code = 402;
-            output.error = '帳密錯誤'
-        } else {
-            req.session.admin = {
-                sid: r1[0].sid,
-                account: r1[0].account,
-            };
-        }
+    output.success = await bcrypt.compare(req.body.password, r1[0].pass_hash);
+    if (!output.success) {
+      // 密碼錯誤
+      output.code = 402;
+      output.error = "帳密錯誤";
+    } else {
+      req.session.admin = {
+        sid: r1[0].sid,
+        account: r1[0].account,
+      };
+    }
 
-        res.json(output);
-    });
+    res.json(output);
+  });
 
-    // JWT登入
-    app.route('/login-jwt')
-    .get(async (req, res)=>{
-        res.render('login-jwt');
-    })
-    .post(async (req, res)=>{
-        const output = {
-            success: false,
-            error: '',
-            code: 0,
-            data: {},
-        };
-        const sql = "SELECT * FROM admins WHERE account=?";
-        const [r1] = await db.query(sql, [req.body.account]);
+// JWT登入
+app
+  .route("/login-jwt")
+  .get(async (req, res) => {
+    res.render("login-jwt");
+  })
+  .post(async (req, res) => {
+    const output = {
+      success: false,
+      error: "",
+      code: 0,
+      data: {},
+    };
+    const sql = "SELECT * FROM admins WHERE account=?";
+    const [r1] = await db.query(sql, [req.body.account]);
 
-        if(! r1.length){
-            // 帳號錯誤
-            output.code = 401;
-            output.error = '帳密錯誤'
-            return res.json(output)
-        }
-        //const row = r1[0];
+    if (!r1.length) {
+      // 帳號錯誤
+      output.code = 401;
+      output.error = "帳密錯誤";
+      return res.json(output);
+    }
+    //const row = r1[0];
 
-        output.success = await bcrypt.compare(req.body.password, r1[0].pass_hash);
-        if(! output.success){
-            // 密碼錯誤
-            output.code = 402;
-            output.error = '帳密錯誤'
-        } else {
-            // 成功登入
-            const token = jwt.sign({
-                sid: r1[0].sid,
-                account: r1[0].account,
-            }, process.env.JWT_SECRET);
+    output.success = await bcrypt.compare(req.body.password, r1[0].pass_hash);
+    if (!output.success) {
+      // 密碼錯誤
+      output.code = 402;
+      output.error = "帳密錯誤";
+    } else {
+      // 成功登入
+      const token = jwt.sign(
+        {
+          sid: r1[0].sid,
+          account: r1[0].account,
+        },
+        process.env.JWT_SECRET
+      );
 
+      output.data = {
+        token,
+        account: r1[0].account,
+      };
+    }
 
-            output.data = {
-                token,
-                account: r1[0].account,
-            };
-
-        }
-
-        res.json(output);
-    });
-
+    res.json(output);
+  });
 
 // 登出
 app.get("/logout", (req, res) => {
-    delete req.session.admin;
-    res.redirect("/");
+  delete req.session.admin;
+  res.redirect("/");
 });
-
 
 // 樣板(ejs)要用render 改成從views找
 // 沒設定檔頭預設HTML
 app.get("/", (req, res) => {
-    res.render("main", { name: "binghan" });
+  res.render("main", { name: "binghan" });
 });
 // -------------static folder-------------
 
@@ -283,12 +287,12 @@ app.use("/joi", express.static("node_modules/joi/dist"));
 // ------------------404------------------
 // use接收所有的方法,querystring亂打都會來這裡
 app.use((req, res) => {
-    res.send(`<h2>找不到頁面 404</h2>
+  res.send(`<h2>找不到頁面 404</h2>
     <img src="/imgs/6c0519f6e0e0d42e458daef829c74ae4.jpg" alt="" width="500px" />`);
 });
 
 // process下的.env設定檔的PORT
 app.listen(process.env.PORT, () => {
-    console.log(`server started:${process.env.PORT}`);
-    console.log({ NODE_ENV: process.env.NODE_ENV });
+  console.log(`server started:${process.env.PORT}`);
+  console.log({ NODE_ENV: process.env.NODE_ENV });
 });
